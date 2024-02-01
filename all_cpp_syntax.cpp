384a22ee34bef294799dc3fa2798a8fccc1903bd
komarcin11 <99415909+komarcin11@users.noreply.github.com>
2024-02-01 19:53:24 +0100

    Add files via upload

---
+++ all_cpp_syntax.cpp
@@ -0,0 +1,9 @@
+// first_cpp_code.cpp
+#include <iostream> // this is calling std lib
+using namespace std; // noooo ide what this code does but without this u need to iner std:: before cout
+
+int main() {
+    cout << "Hello world\n"; //cout means C output that is the code to output the values, after every statement u need to add ;
+    std:: cout<<"hello world\n"; // the std:: is needed if u dong include "using namespace std;" before the main function
+}
+
